3c911fea3536386912c2849aaecc77c0cbb8ca25
vipin2210 <73049843+vipin2210@users.noreply.github.com>
2021-05-02 11:20:42 +0530

    Delete script2.js

--- script2.js
+++
@@ -1,8 +0,0 @@
-"use strict"
-var box = document.getElementById('box');
-var clickcount = document.getElementById('click-btn');
-var count = 0;
-box.addEventListener('click', function () {
-    count++;
-    clickcount.innerHTML = count;
-});
